2370cc07f5738db0b7b9502ebc6cc9995c31936a
DevByteR <mdromman417@gmail.com>
2026-03-31 09:06:52 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   
 
   return (
     <>
-     <h3 className='text-5xl'>Hello babu  </h3>
+     <NavBar/>
     </>
   )
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+const NavBar = () => {
+  return (
+    <div>
+      <div className=" shadow-sm ">
+        <div className="container mx-auto navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a href="#">Products</a>
+              </li>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <h2 className="text-3xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-extrabold">DigiTools</h2>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 flex gap-8 font-semibold">
+             <li>
+                <a href="#">Products</a>
+              </li>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+          </ul>
+        </div>
+        <div className="navbar-end gap-5"> 
+          <FiShoppingCart />
+         <a className="font-semibold hover:btn" href="#">Login</a>
+          <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">Get Started</a>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
