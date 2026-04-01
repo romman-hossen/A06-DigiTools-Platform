a5f5b5d3176537a473590bea6372ae31456b842e
DevByteR <mdromman417@gmail.com>
2026-04-01 21:30:50 +0600

    footer section added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,55 +1,69 @@
 import React from "react";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaSquareFacebook, FaXTwitter } from "react-icons/fa6";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <div className="bg-neutral">
-      <footer className="max-w-7xl mx-auto footer sm:footer-horizontal text-white  p-10">
+      <footer className="max-w-7xl mx-auto footer sm:footer-horizontal text-white p-10 md:p-0 md:pt-30 md:pb-20">
         <aside>
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            className="fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
+          <h3 className="text-5xl font-extrabold pb-4">DigiTools</h3>
+          <p className="text-[16px] text-gray-400">
+           Premium digital tools for creators,<br /> professionals, and businesses. Work smarter <br /> with our suite of powerful tools.
           </p>
         </aside>
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <h6 className="footer-title">Product</h6>
+          <a className="link link-hover">Features</a>
+          <a className="link link-hover">Pricing</a>
+          <a className="link link-hover">Templates</a>
+          <a className="link link-hover">Integrations</a>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
           <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <a className="link link-hover">Blog</a>
+          <a className="link link-hover">Careers</a>
+          <a className="link link-hover">Press</a>
         </nav>
         <aside>
-             <h6 className="footer-title">Legal fgggffffffffffffffffbhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+             <h6 className="footer-title">Resources </h6>
+          <a className="link link-hover">Documentation</a>
+          <a className="link link-hover">Help Center</a>
+          <a className="link link-hover">Community</a>
+          <a className="link link-hover">Contact</a>
 
         </aside>
+        <aside>
+          <h3 className="text-xl font-medium">Social Links</h3>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="w-10 h-10 bg-white flex items-center justify-center rounded-full">
+              {/* <AiFillInstagram /> */}
+              <RiInstagramFill className="text-black text-2xl"/>
+
+              {/* <AiFillInstagram */}
+            </div>
+            <div className="w-10 h-10 bg-white flex items-center justify-center rounded-full">
+              <FaSquareFacebook className="text-black text-2xl"/>
+            </div>
+            <div className="w-10 h-10 bg-white flex items-center justify-center rounded-full">
+              <FaXTwitter className="text-black text-2xl"/>
+
+            </div>
+          </div>
+        </aside>
+   
       </footer>
+      <hr className="max-w-7xl mx-auto items-center"/>
+      <div className="text-gray-400 flex flex-col md:flex-row gap-6 md:gap-0 justify-between items-center max-w-7xl mx-auto py-8 px-8 md:px-0">
+        <p>© 2026 Digitools. All rights reserved.</p>
+        <div className="flex items-center gap-4">
+          <span>Privacy Policy</span>
+          <span>Terms of Service </span>
+          <span>Cookies</span>
+        </div>
+      </div>
     </div>
   );
 };
